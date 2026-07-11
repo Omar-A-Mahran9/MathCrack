750df2163383a816fff578f03c1fb3941397da51
Ahmed Zahran <amzahran@azhar.edu.eg>
2026-07-11 17:21:04 +0300

    Add user books navigation and cleanup gitignore

--- resources/views/themes/default/layouts/back/headerUsers.blade.php
+++ resources/views/themes/default/layouts/back/headerUsers.blade.php
@@ -1,17 +1,23 @@
-
-
-
 <li class="nxl-item nxl-caption">
     <label>-----</label>
 </li>
-<li class="nxl-item {{request()->is('dashboard.users.courses') ? 'active' : ''}}">
-    <a href="{{route('dashboard.users.courses')}}" class="nxl-link">
+
+<li class="nxl-item {{ request()->routeIs('dashboard.users.courses') ? 'active' : '' }}">
+    <a href="{{ route('dashboard.users.courses') }}" class="nxl-link">
         <span class="nxl-micon"><i class="feather-book"></i></span>
         <span class="nxl-mtext">@lang('l.Courses')</span>
     </a>
 </li>
-<li class="nxl-item {{request()->is('dashboard.users.invoices') ? 'active' : ''}}">
-    <a href="{{route('dashboard.users.invoices')}}" class="nxl-link">
+
+<li class="nxl-item {{ request()->routeIs('dashboard.users.books') || request()->routeIs('dashboard.users.books-show') || request()->routeIs('dashboard.users.books-purchase') ? 'active' : '' }}">
+    <a href="{{ route('dashboard.users.books') }}" class="nxl-link">
+        <span class="nxl-micon"><i class="feather-book-open"></i></span>
+        <span class="nxl-mtext">Books</span>
+    </a>
+</li>
+
+<li class="nxl-item {{ request()->routeIs('dashboard.users.invoices') ? 'active' : '' }}">
+    <a href="{{ route('dashboard.users.invoices') }}" class="nxl-link">
         <span class="nxl-micon"><i class="feather-file-text"></i></span>
         <span class="nxl-mtext">@lang('l.Invoices')</span>
     </a>
